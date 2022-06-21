35a9a5e9193525482f1691ac4fb8a0065ec0ab74
YAGAI <c0a20155db@edu.teu.ac.jp>
2022-06-21 14:53:51 +0900

    問6

--- ex02/calc.py
+++ ex02/calc.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     entry.grid(row=0, column=0, columnspan=3)
     
     r, c = 1, 0
-    for x in range(9, -1, -1):
+    for i, x in enumerate([i for i in range(9, -1, -1)]+["+"]):
         btn = tk.Button(root, 
                        text=f"{x}", 
                         width=4, 
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         btn.bind("<1>", button_click)
         btn.grid(row=r, column=c)
         c += 1
-        if (x-1)%3 == 0:
+        if (i+1)%3 == 0:
             r += 1
             c = 0
 
