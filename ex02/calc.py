9c5961107e6cf21dcc4bc04091392f15dbc3eada
YAGAI <c0a20155db@edu.teu.ac.jp>
2022-06-21 14:42:15 +0900

    問4

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,12 +6,21 @@ def button_click(event):
     num = btn["text"]
     tkm.showinfo("", f"{x}のボタンがクリックされました")
 
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("300x500")
     root.title("電卓")
     
-    r, c = 0, 0
+    entry = tk.Entry(root, 
+                    justify="right",   
+                    width=10, 
+                    font=("Times New Roman", 40)
+                )
+    entry.grid(row=0, column=0, columnspan=3)
+    
+    r, c = 1, 0
     for x in range(9, -1, -1):
         btn = tk.Button(root, 
                        text=f"{x}", 
